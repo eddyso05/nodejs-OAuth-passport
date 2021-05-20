02ac310f2156d41a9a7fb7223e0363362531747b
eddy siow <eddysiow0530@hotmail.com>
2021-05-20 16:54:04 +0800

    update key

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     google: {
-        clietnID:'530730025088-3on8hdvurb1n9v6nmk7u9s2ecv89ci90.apps.googleusercontent.com',
-        clientSecret:'BkLEQzemHT1JM7M2MTcnRZXx'
+        clietnID:'ur client id',
+        clientSecret:'ur client secret'
     },
     mongodb:{
         dbURI:'mongodb://localhost:27017/oauth-test'
